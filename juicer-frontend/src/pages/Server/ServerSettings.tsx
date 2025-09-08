28e9a92c40b3215ffd325898913baa9f192ac570
MareNectaris <30724686+MareNectaris@users.noreply.github.com>
2025-09-08 18:21:45 +0900

    fix conditional rendering

--- juicer-frontend/src/pages/Server/ServerSettings.tsx
+++ juicer-frontend/src/pages/Server/ServerSettings.tsx
@@ -1,15 +1,13 @@
 import AddIcon from "@mui/icons-material/Add";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import DeleteIcon from "@mui/icons-material/Delete";
-import SettingsIcon from "@mui/icons-material/Settings";
-import SyncIcon from "@mui/icons-material/Sync";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { Suspense } from "react";
 import { useNavigate, useSearchParams } from "react-router";
-import serverPlaceholderIcon from "../../assets/server_icon_placeholder.png";
 import { _fetchServerData } from "../../queries/queries";
+import type { Category, Tag } from "../../types/types";
 import { Button, InlineButton } from "../../ui/components/Button";
-import { Card, ResponsiveCard } from "../../ui/components/Card";
+import { ResponsiveCard } from "../../ui/components/Card";
 import { Chip } from "../../ui/components/Chip";
 import { FullPageBase } from "../../ui/components/FullPageBase";
 import { Loading } from "../Loading/Loading";
@@ -65,7 +63,65 @@ export const ServerSettings = () => {
 								gap: "12px",
 							}}
 						>
-							<h2 css={{ margin: 0 }}>카테고리</h2>
+							<div
+								css={{
+									display: "flex",
+									flexDirection: "row",
+								}}
+							>
+								<h2 css={{ margin: 0, flex: 1 }}>카테고리</h2>
+								<Button
+									css={{
+										background: "#5865F2",
+										display: "flex",
+										alignItems: "center",
+										gap: "8px",
+									}}
+								>
+									<AddIcon css={{ width: "16px", height: "16px" }} />
+									카테고리 추가
+								</Button>
+							</div>
+							{_serverData.data?.server_data_db.categories?.length || (
+								<div css={{ color: "rgba(255, 255, 255, 0.5)" }}>
+									서버에 카테고리가 없습니다.
+								</div>
+							)}
+							{!!_serverData.data?.server_data_db.categories?.length && (
+								<div
+									css={{ display: "flex", flexDirection: "row", gap: "6px" }}
+								>
+									{_serverData.data?.server_data_db.categories?.map(
+										(category: Category) => (
+											<Chip
+												key={category.id}
+												css={{
+													display: "flex",
+													flexDirection: "row",
+													gap: "4px",
+												}}
+											>
+												<InlineButton
+													css={{
+														height: "100%",
+														alignItems: "center",
+														justifyContent: "center",
+													}}
+												>
+													<DeleteIcon
+														css={{
+															width: "16px",
+															height: "16px",
+															color: "#FFF",
+														}}
+													/>
+												</InlineButton>
+												{category.name}
+											</Chip>
+										),
+									)}
+								</div>
+							)}
 						</div>
 						<div
 							css={{
@@ -75,37 +131,68 @@ export const ServerSettings = () => {
 								gap: "12px",
 							}}
 						>
-							<h2 css={{ margin: 0 }}>태그</h2>
-							<div css={{ display: "flex", flexDirection: "row", gap: "6px" }}>
-								<Chip
+							<div
+								css={{
+									display: "flex",
+									flexDirection: "row",
+								}}
+							>
+								<h2 css={{ margin: 0, flex: 1 }}>태그</h2>
+								<Button
 									css={{
+										background: "#5865F2",
 										display: "flex",
-										flexDirection: "row",
-										gap: "4px",
+										alignItems: "center",
+										gap: "8px",
 									}}
 								>
-									<InlineButton
+									<AddIcon css={{ width: "16px", height: "16px" }} />
+									태그 추가
+								</Button>
+							</div>
+							<div css={{ display: "flex", flexDirection: "row", gap: "6px" }}>
+								{_serverData.data?.server_data_db.tags?.length || (
+									<div css={{ color: "rgba(255, 255, 255, 0.5)" }}>
+										서버에 태그가 없습니다.
+									</div>
+								)}
+								{!!_serverData.data?.server_data_db.tags?.length && (
+									<div
 										css={{
-											height: "100%",
-											alignItems: "center",
-											justifyContent: "center",
+											display: "flex",
+											flexDirection: "row",
+											gap: "6px",
 										}}
 									>
-										<DeleteIcon
-											css={{ width: "16px", height: "16px", color: "#FFF" }}
-										/>
-									</InlineButton>
-									태그1
-								</Chip>
-								<Chip>태그2</Chip>
-								<Chip>태그3</Chip>
-								<Chip>태그4</Chip>
-								<Chip>태그5</Chip>
-								<Chip>태그6</Chip>
-								<Chip>태그7</Chip>
-								<Chip>태그8</Chip>
-								<Chip>태그9</Chip>
-								<Chip>태그10</Chip>
+										{_serverData.data?.server_data_db.tags?.map((tag: Tag) => (
+											<Chip
+												key={tag.id}
+												css={{
+													display: "flex",
+													flexDirection: "row",
+													gap: "4px",
+												}}
+											>
+												<InlineButton
+													css={{
+														height: "100%",
+														alignItems: "center",
+														justifyContent: "center",
+													}}
+												>
+													<DeleteIcon
+														css={{
+															width: "16px",
+															height: "16px",
+															color: "#FFF",
+														}}
+													/>
+												</InlineButton>
+												{tag.name}
+											</Chip>
+										))}
+									</div>
+								)}
 							</div>
 						</div>
 					</div>
