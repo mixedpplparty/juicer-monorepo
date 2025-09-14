8b24a4a36423d62701d1f03915140a126b4da51c
MareNectaris <30724686+MareNectaris@users.noreply.github.com>
2025-09-14 20:09:48 +0900

    add dotenv and dotenv-webpack as deps

--- juicer-frontend/vite.config.ts
+++ juicer-frontend/vite.config.ts
@@ -2,9 +2,11 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), svgr()],
-	server: {
-		allowedHosts: ["juicer.r1c.cc"],
-	},
+export default defineConfig(({ mode }) => {
+	return {
+		plugins: [react(), svgr()],
+		server: {
+			allowedHosts: ["juicer.r1c.cc"],
+		},
+	};
 });
