7912bc51afca85ec69f6635b0402b2a02a10b078
MareNectaris <30724686+MareNectaris@users.noreply.github.com>
2025-09-15 14:57:19 +0900

    add auth message

--- juicer-frontend/src/pages/Loading/Loading.tsx
+++ juicer-frontend/src/pages/Loading/Loading.tsx
@@ -2,14 +2,14 @@ import { ResponsiveCard } from "../../ui/components/Card";
 import { FullPageBase } from "../../ui/components/FullPageBase";
 import { Spinner } from "../../ui/components/Spinner";
 
-export const Loading = () => {
+export const Loading = ({ message }: { message?: string | undefined }) => {
 	return (
 		<FullPageBase>
 			<ResponsiveCard
 				css={{ alignItems: "center", justifyContent: "center", gap: "12px" }}
 			>
 				<Spinner />
-				<h3 css={{ margin: 0 }}>Loading...</h3>
+				<h3 css={{ margin: 0 }}>{message || "Loading..."}</h3>
 			</ResponsiveCard>
 		</FullPageBase>
 	);
