ac7a0d81d9a80a6ca8109ab8180419834cf1c190
MareNectaris <30724686+MareNectaris@users.noreply.github.com>
2025-09-21 17:51:20 +0900

    initialize Drizzle

---
+++ src/server/drizzle.config.ts
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+	out: "./drizzle",
+	schema: "./src/db/schema",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
+	},
+});
---
+++ src/server/src/db/index.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+
+const pool = new Pool({
+	host: process.env.POSTGRES_HOST,
+	port: parseInt(process.env.POSTGRES_PORT as string),
+	user: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+	database: process.env.POSTGRES_DB,
+	idleTimeoutMillis: 0,
+	allowExitOnIdle: false,
+});
+export const db = drizzle({ client: pool });
