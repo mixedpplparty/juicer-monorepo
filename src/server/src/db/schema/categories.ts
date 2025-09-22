f6768f8e6326c366d067c2f4a59c2ab46fac18b0
MareNectaris <30724686+MareNectaris@users.noreply.github.com>
2025-09-22 17:06:35 +0900

    update relations

--- src/server/src/db/schema/categories.ts
+++ src/server/src/db/schema/categories.ts
@@ -1,4 +1,6 @@
+import { relations } from "drizzle-orm";
 import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { games } from "./games.ts";
 import { servers } from "./servers.ts";
 
 export const categories = pgTable("categories", {
@@ -8,3 +10,12 @@ export const categories = pgTable("categories", {
 		.references(() => servers.serverId, { onDelete: "cascade" }),
 	name: varchar("name", { length: 100 }).notNull(),
 });
+
+// categories -> server, games
+export const categoriesRelations = relations(categories, ({ one, many }) => ({
+	server: one(servers, {
+		fields: [categories.serverId],
+		references: [servers.serverId],
+	}),
+	games: many(games),
+}));
--- src/server/src/db/schemas.ts
+++ src/server/src/db/schemas.ts
@@ -1,7 +1,5 @@
 export * from "./schema/categories.js";
 export * from "./schema/games.js";
-export * from "./schema/games-channels.js";
-export * from "./schema/games-roles.js";
 export * from "./schema/role-categories.js";
 export * from "./schema/roles.js";
 export * from "./schema/servers.js";
