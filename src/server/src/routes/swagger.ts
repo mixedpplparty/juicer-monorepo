a408b4e653b990a00d62d718ef4fd28c7d36b1bf
MareNectaris <30724686+MareNectaris@users.noreply.github.com>
2025-09-24 19:36:46 +0900

    finish swagger for /discord/auth, /discord/user

--- src/server/src/routes/swagger.ts
+++ src/server/src/routes/swagger.ts
@@ -9,7 +9,88 @@ const openApiDoc = {
 		version: "0.0.1",
 		description: "juicer API",
 	},
-	paths: {},
+	paths: {
+		"/discord": {
+			"/auth": {
+				"/callback": {
+					get: {
+						summary: "Callback for Discord OAuth2",
+						description:
+							"Callback page from to be redirected from https://discord.com/oauth2/authorize",
+						parameters: {
+							code: {
+								type: "string",
+								description:
+									"Code from Discord OAuth2(should be automatically filled in by Discord)",
+								required: true,
+							},
+						},
+						responses: {
+							"200": {
+								description:
+									"Access token and refresh token are set in cookies. Redirected to the page specified in .env.REDIRECT_AFTER_SIGN_IN_URI",
+							},
+						},
+					},
+				},
+				"/refresh": {
+					post: {
+						summary: "Refresh Discord OAuth2 token",
+						description: "Refresh Discord OAuth2 token",
+						responses: {
+							"200": {
+								description:
+									"New access token and refresh token are set in cookies. Redirected to the page specified in .env.REDIRECT_AFTER_SIGN_IN_URI",
+							},
+						},
+					},
+				},
+				"/revoke": {
+					post: {
+						summary: "Revoke Discord OAuth2 token('Sign Out')",
+						description: "Revoke Discord OAuth2 token('Sign Out')",
+						responses: {
+							"200": {
+								description:
+									"Access token and refresh token are deleted from cookies. Redirected to the page specified in .env.REDIRECT_AFTER_SIGN_IN_URI",
+							},
+						},
+					},
+				},
+			},
+			"/user": {
+				"/me": {
+					get: {
+						summary: "Get user data and all servers user and bot are in",
+						description: "Get user data and all servers user and bot are in",
+					},
+					responses: {
+						"200": {
+							description: "User data and all servers user and bot are in",
+							content: {
+								"application/json": {
+									schema: {
+										type: "object",
+										properties: {
+											userData: {
+												type: "APIUser",
+											},
+											guilds: {
+												type: "array",
+												items: {
+													type: "Guild",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
 };
 app.get("/", (c) => {
 	return c.json(openApiDoc);
