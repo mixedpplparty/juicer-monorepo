8686a53342a04492ad0e1644d420700da02fc5ae
MareNectaris <30724686+MareNectaris@users.noreply.github.com>
2025-09-25 17:02:24 +0900

    fix more typos

--- src/server/src/db/schemas.ts
+++ src/server/src/db/schemas.ts
@@ -1,6 +1,5 @@
 export * from "./schema/categories.js";
 export * from "./schema/games.js";
-export * from "./schema/role-categories.js";
 export * from "./schema/roles.js";
 export * from "./schema/servers.js";
 export * from "./schema/tags.js";
