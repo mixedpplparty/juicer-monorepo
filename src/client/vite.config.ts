9e1425d115004cd498f5dba2028e071114cbd02f
MareNectaris <30724686+MareNectaris@users.noreply.github.com>
2026-07-17 14:03:48 +0900

    init: empty client repo

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,12 +1,10 @@
-import react from "@vitejs/plugin-react";
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import svgr from "vite-plugin-svgr";
-// https://vite.dev/config/
-export default defineConfig(() => {
-	return {
-		plugins: [react(), svgr()],
-		server: {
-			allowedHosts: ["juicer.r1c.cc"],
-		},
-	};
+
+export default defineConfig({
+  plugins: [tailwindcss(), reactRouter()],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
