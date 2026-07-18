05a8755ef2a4fb97f764f6a73700e8c167d38fec
MareNectaris <30724686+MareNectaris@users.noreply.github.com>
2026-07-18 18:03:35 +0900

    feat: HydrateFallback for router(base code only)

---
+++ src/client/src/components/auth-loading.tsx
@@ -0,0 +1,5 @@
+export function AuthLoading() {
+	return <div>로그인 상태를 확인하는 중..</div>;
+}
+
+export default AuthLoading;
--- src/client/src/router.tsx
+++ src/client/src/router.tsx
@@ -4,6 +4,7 @@ import {
 	Outlet,
 	redirect,
 } from "react-router";
+import AuthLoading from "./components/auth-loading";
 import RouteErrorBoundary from "./components/error-boundary";
 import _isAuthenticated from "./modules/auth/auth-check";
 import SignInPage from "./pages/auth/login";
@@ -25,7 +26,6 @@ async function _authOnlyLoader({ request }: LoaderFunctionArgs) {
 		const redirectTo = `${url.pathname}${url.search}`;
 		throw redirect(`/login?redirectTo=${encodeURIComponent(redirectTo)}`);
 	}
-
 	return null;
 }
 
@@ -36,6 +36,7 @@ function RouteOutlet() {
 export const router = createBrowserRouter([
 	{
 		Component: RouteOutlet,
+		HydrateFallback: AuthLoading,
 		ErrorBoundary: RouteErrorBoundary,
 		children: [
 			{
